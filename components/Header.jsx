55659a65f789663adf2c0386851e36f5750f5018
Mesquita Dev <lmesquita2k@gmail.com>
2023-06-26 21:43:48 -0300

    Dashboard - React, Next.js, Tailwind and others

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='flex justify-between px-4 pt-4'>
+      <h2>Dashboard</h2>
+      <h2>Welcome Back, Lucas</h2>
+    </header>
+  )
+}
+
+export default Header
